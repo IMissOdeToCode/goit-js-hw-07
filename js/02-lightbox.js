7e9cd0935f3bda624935401072c9431371079470
Denys Konopatkin <k.denis.aa@gmail.com>
2022-11-13 21:05:14 +0200

    part 02 is done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+// references
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
 
-console.log(galleryItems);
+const getItemTemplate = (Item) => {
+  const { preview, original, description } = Item;
+
+  // prettier-ignore
+  const templateString =
+        `<a class="gallery__item" href="${original}" data-arr="test">
+            <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`;
+
+  return templateString;
+};
+
+// making gallery markup
+const pictures = galleryItems.map((item) => getItemTemplate(item));
+refs.gallery.insertAdjacentHTML('beforeend', pictures.join(''));
+
+// usage SimpleLightbox
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
